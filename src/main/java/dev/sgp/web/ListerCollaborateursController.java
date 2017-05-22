d0b9eabdba09207f181b15942890214ba791f81c
moussbgf DTA <mboughazif@gmail.com>
2017-05-22 14:57:32 +0200

    TP 03 - Premiere page JSP

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,8 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,34 +30,33 @@ public class ListerCollaborateursController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 
-		response.getWriter().write("Salut ListerCollaborateursController !");
-
-		// récupère la valeur d'un paramètre dont le nom est avecPhoto
-
-		String avecPhotoParam = request.getParameter("avecPhoto");
-
-		// récupère la valeur d'un paramètre dont le nom est departement
-
-		String departementParam = request.getParameter("departement");
-
-		response.setContentType("text/html");
-
-		// code HTML écrit dans le corps de la réponse
-
-		response.getWriter().write("<h1>Liste des collaborateurs</h1>"
-
-				+ "<ul>"
-
-				+ "<li>avecPhoto=" + avecPhotoParam + "</li>"
-
-				+ "<li>departement=" + departementParam + "</li>"
-
-				+ "</ul>");
-
-		// response.getWriter().append("Served at:
-		// ").append(request.getContextPath());
+//		response.getWriter().write("Salut ListerCollaborateursController !");
+//
+//		// récupère la valeur d'un paramètre dont le nom est avecPhoto
+//
+//		String avecPhotoParam = request.getParameter("avecPhoto");
+//
+//		// récupère la valeur d'un paramètre dont le nom est departement
+//
+//		String departementParam = request.getParameter("departement");
+//
+//		response.setContentType("text/html");
+//
+//		// code HTML écrit dans le corps de la réponse
+//
+//		response.getWriter().write("<h1>Liste des collaborateurs</h1>"
+//
+//				+ "<ul>"
+//
+//				+ "<li>avecPhoto=" + avecPhotoParam + "</li>"
+//
+//				+ "<li>departement=" + departementParam + "</li>"
+//
+//				+ "</ul>");
+		request.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
+		request.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp")
+		.forward(request, response);
 	}
 
 	/**
