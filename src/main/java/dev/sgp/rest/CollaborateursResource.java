12a2d7e79a9dba0b1e5618bdd7ee91fa342c8227
moussbgf DTA <mboughazif@gmail.com>
2017-05-29 18:07:18 +0200

    TP Jax-RS -OK- gestion des erreurs http (collaborateur resource)

--- src/main/java/dev/sgp/rest/CollaborateursResource.java
+++ src/main/java/dev/sgp/rest/CollaborateursResource.java
@@ -1,5 +1,6 @@
 package dev.sgp.rest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,85 +15,133 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import com.sun.mail.imap.protocol.Status;
+
 import dev.sgp.entite.Collaborateur;
 import dev.sgp.service.CollaborateurService;
 
 @Path("/collaborateurs")
 public class CollaborateursResource {
-	
-	@EJB private CollaborateurService collabService;
-	
-//	@GET
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public List<Collaborateur> list() {
-//
-//	List<Collaborateur> collaborateurs = collabService.listerCollaborateurs();
-//	
-//	return collaborateurs;
-//	}
-	
+
+	@EJB
+	private CollaborateurService collabService;
+
+	// @GET
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public List<Collaborateur> list() {
+	//
+	// List<Collaborateur> collaborateurs =
+	// collabService.listerCollaborateurs();
+	//
+	// return collaborateurs;
+	// }
+
 	@GET
 	@Path("/{matricule}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Collaborateur findByMatricule(@PathParam("matricule") String matricule) {
 
-	Collaborateur collaborateur = collabService.find(matricule);
-	
-	return collaborateur;
+		Collaborateur collaborateur = collabService.find(matricule);
+
+		return collaborateur;
 	}
-	
+
 	@GET
 	@Path("/{matricule}/banque")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Map<String, String> findByMatriculeBanque(@PathParam("matricule") String matricule) {
 
-	Collaborateur collaborateur = collabService.find(matricule);
-	
-	Map<String, String> banqueWrap = new HashMap<>();
-	
-	banqueWrap.put("Banque", collaborateur.getBanque());
-	banqueWrap.put("Bic", collaborateur.getBic());
-	banqueWrap.put("Iban", collaborateur.getIban());
+		Collaborateur collaborateur = collabService.find(matricule);
 
-	
-	return banqueWrap;
+		Map<String, String> banqueWrap = new HashMap<>();
+
+		banqueWrap.put("Banque", collaborateur.getBanque());
+		banqueWrap.put("Bic", collaborateur.getBic());
+		banqueWrap.put("Iban", collaborateur.getIban());
+
+		return banqueWrap;
 	}
-	
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	
-	public List<Collaborateur> list(@QueryParam("departement") @DefaultValue("0") Integer departementId ) {
-		
+
+	public List<Collaborateur> list(@QueryParam("departement") @DefaultValue("0") Integer departementId) {
+
 		List<Collaborateur> collaborateurs;
-		
-		if(departementId != 0) {
+
+		if (departementId != 0) {
 			collaborateurs = collabService.findByDpt(departementId);
 		} else {
 			collaborateurs = collabService.listerCollaborateurs();
 		}
 		return collaborateurs;
 	}
-	
+
 	@PUT
 	@Path("/{matricule}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void modify(Collaborateur newCollab, @PathParam("matricule") String matricule) {
-		
-			collabService.modifierCollaborateur(matricule, newCollab);
 
-		
+		collabService.modifierCollaborateur(matricule, newCollab);
+
 	}
-	
-	
+
 	@PUT
 	@Path("/{matricule}/banque")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void modifyBanque(Map<String, String> banqueWrap, @PathParam("matricule") String matricule) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response modifyBanque(Map<String, String> banqueWrap, @PathParam("matricule") String matricule) {
 		
-			collabService.modifierCollaborateurBanque(matricule, banqueWrap);
+		//Serie de tests à deplacer vers le service 
+		
+		List<String> menquant = new ArrayList<String>();
+		
+		ResponseBuilder builder;
 
+		if (banqueWrap.get("Banque") == null || banqueWrap.get("Banque").isEmpty() ) {
+			menquant.add("Banque");
+		}
+
+		if ((banqueWrap.get("Bic") == null || banqueWrap.get("Bic").isEmpty()) ) {
+			menquant.add("Bic");
+		}
+
+		if ((banqueWrap.get("Iban") == null || banqueWrap.get("Iban").isEmpty())) {
+			menquant.add("Iban");
+		}
 		
+		
+		if(menquant.size() == 0) {
+			collabService.modifierCollaborateurBanque(matricule, banqueWrap);
+			builder = Response.ok("texte");
+
+			builder.language("fr")
+
+			.header("Content-type", "text/html");
+
+			return builder.build();
+			
+		} else {
+			
+			builder = Response.status(Response.Status.EXPECTATION_FAILED);
+			
+			
+			builder.language("fr")
+
+			
+			
+			.entity("champs menquant : " + menquant)
+			
+			;
+
+			return builder.build();
+		}
+
+
+
 	}
-	
+
 }
